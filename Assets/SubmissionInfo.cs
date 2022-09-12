02b809316dd6444994fef31b529d4fda44c7e84c
Kenna7 <lucaskenna535@gmail.com>
2022-09-12 13:15:49 +1000

    added team member names and placeholder game name

--- Assets/SubmissionInfo.cs
+++ Assets/SubmissionInfo.cs
@@ -9,25 +9,24 @@ public static class SubmissionInfo
     // for extra details related to use of this file.
 
     // URL to your group's project 2 repository on GitHub.
-    public static readonly string RepoURL = "https://github.com/COMP30019/project-2-";
+    public static readonly string RepoURL = "https://github.com/COMP30019/project-2-tony-pizza";
     
     // Come up with a team name below (plain text, no more than 50 chars).
-    public static readonly string TeamName = "Replace with your team name";
+    public static readonly string TeamName = "Tony Pizza";
     
     // List every team member below. Ensure student names/emails match official
     // UniMelb records exactly (e.g. avoid nicknames or aliases).
     public static readonly TeamMember[] Team = new[]
     {
-        new TeamMember("Student Name 1", "...@student.unimelb.edu.au"),
-        new TeamMember("Student Name 2", "...@student.unimelb.edu.au"),
-        new TeamMember("Student Name 3", "...@student.unimelb.edu.au"),
-        // Remove the following line if you have a group of 3
-        new TeamMember("Student Name 4", "...@student.unimelb.edu.au"), 
+        new TeamMember("Lucas Kenna", "lkenna@student.unimelb.edu.au"),
+        new TeamMember("Sebastian Rey-Fleming", "sreyfleming@student.unimelb.edu.au"),
+        new TeamMember("Yannick Ullisch", "yullisch@student.unimelb.edu.au"),
+        new TeamMember("Jenny Aas", "jennyjosefin@student.unimelb.edu.au"), 
     };
 
     // This may be a "working title" to begin with, but ensure it is final by
     // the video milestone deadline (plain text, no more than 50 chars).
-    public static readonly string GameName = "Replace with your game name";
+    public static readonly string GameName = "Scary Escape";
 
     // Write a brief blurb of your game, no more than 200 words. Again, ensure
     // this is final by the video milestone deadline.
